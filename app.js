f700f77179a7e4519dbc253fb22ec0d7a9fbc42e
amjad-awan <73718759+amjad-awan@users.noreply.github.com>
2021-10-04 11:30:36 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,32 +3,23 @@ const slidBtn2 = document.querySelector(".btn2");
 const formArea = document.querySelector(".form-area");
 const formArea2 = document.querySelector(".form-area2")
 const firstForm = document.querySelector(".first-form")
-const secForm = document.querySelector(".sec-form")
+const secForm = document.querySelector(".sec-form");
+
 
 slidBtn1.addEventListener("click", function(){
+    formArea.classList.remove("form-area-transform");
+    formArea2.classList.remove("form-area2-transform");
 slidBtn2.classList.remove("click");
 slidBtn1.classList.add("click");
-formArea2.classList.remove("normal-display");
-    secForm.innerHTML=firstForm.innerHTML;
+
 })
 
-/*slidBtn2.addEventListener("click", function(){
-    formArea2.classList.add("form-area2-normal");
-})*/
+
 
 
 slidBtn2.addEventListener("click", function(){
-    formArea2.style.display="block";
-    //formArea2[2].style.display="none";
-    firstForm.innerHTML=secForm.innerHTML;
-    //secForm.innerHTML="";
+    formArea.classList.add("form-area-transform");
+    formArea2.classList.add("form-area2-transform");
     slidBtn1.classList.remove("click");
 slidBtn2.classList.add("click");
 })
-
-
-
-
-
-
-
